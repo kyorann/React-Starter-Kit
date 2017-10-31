a9f64764f121b6bf9624219c40faa8915358fdac
bonusrk <word0bearer@gmail.com>
2017-10-31 19:00:14 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const App = props => {
+
+
+  return (
+    <div>Hello, World!</div>
+  )
+}
+
+App.PropTypes={
+
+}
+
+export default App
